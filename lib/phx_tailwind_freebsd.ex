571e84bdc32732e97bbb6dbdd983570cfa2b8d03
Harshad Sharma <harshad@sharma.io>
2024-04-01 23:29:27 +0530

    Add docs.

--- lib/phx_tailwind_freebsd.ex
+++ lib/phx_tailwind_freebsd.ex
@@ -1,5 +1,17 @@
 defmodule PhxTailwindFreebsd do
   @moduledoc """
   Workaround to install Tailwind binary on FreeBSD.
+
+  Add `phx_tailwind_freebsd` to your list of dependencies in `mix.exs`:
+
+  ```elixir
+  def deps do
+    [
+      {:phx_tailwind_freebsd, "~> 0.1.0", runtime: Mix.env() == :dev}
+    ]
+  end
+  ```
+
+  Run `mix tailwind.install_freebsd` in the project directory.
   """
 end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule PhxTailwindFreebsd.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: [
+        main: "PhxTailwindFreebsd",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ],
+      source_url: "https://github.com/hiway/phx_tailwind_freebsd/"
     ]
   end
 
@@ -18,6 +26,19 @@ defmodule PhxTailwindFreebsd.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Workaround to install Tailwind binary on FreeBSD."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hiway/phx_tailwind_freebsd/"}
+    ]
   end
 end
